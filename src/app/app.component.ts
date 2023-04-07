775de9a2750b9da78736c92ceff969515d930452
Fozil Zayn <fozilzayniddinov7777@gmail.com>
2023-04-07 15:56:41 +0500

    Angular front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { BookService } from './services/book.service';
 import { Component } from '@angular/core';
+import { Book } from './models/book';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LibraryUI';
+  books: Book[] = [];
+  bookToEdit?: Book
+
+  constructor(private bookService: BookService) {}
+
+  ngOnInit(): void{
+    this.bookService.getBooks().subscribe((res: Book[]) => (this.books = res));
+  }
+
+  updateBookList(books: Book[]){
+    this.books = books;
+  }
+
+  initNewBook() {
+    this.bookToEdit = new Book();
+  }
+
+  editBook(book: Book) {
+    this.bookToEdit = book;
+  }
 }
